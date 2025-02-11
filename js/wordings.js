b05322cd035d228415cbcaad5289d1a55f25c264
Sebas Lon <sebaslondonomesa@hotmail.com>
2025-02-11 14:50:01 -0500

    first commit

---
+++ js/wordings.js
@@ -0,0 +1,7 @@
+export const wordings = {
+    welcomeTitle: "✨¡Hola, lind@! ¡Bienvenid@ a la Tierra de los Teddy! ✨",
+    welcomeSubtitle: "¡Solo las personas súper tiernas pueden entrar! 🐻💗",
+    surpriseTitle: "✨ ¡Sorpresa, precios@! ✨",
+    surpriseMessage: "¡Aquí tienes un abrazo especial de tu amigo teddy! 🤗💖",
+    nextButtonText: "🌈 ¡Siguiente aventura! 🌈"
+};
